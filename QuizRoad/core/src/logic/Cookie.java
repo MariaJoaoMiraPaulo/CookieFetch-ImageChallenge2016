169bae4f52a8aab3b4638f837e5ad61e7e610f06
Antonio-Melo <happyd3nt@gmail.com>
2016-11-04 18:08:04 +0000

    Added Cookie&Plate Classes

---
+++ QuizRoad/core/src/logic/Cookie.java
@@ -0,0 +1,22 @@
+package logic;
+
+/**
+ * Created by antoniomelo on 11/4/16.
+ */
+
+public class Cookie {
+
+    public enum State {
+        IDLE, BTHROWING, INPLATE
+    }
+
+    public enum Type {
+        BLUE,GREEN,BROWN
+    }
+    public static int value = 5;
+
+    public Cookie(){
+        //Type type;
+
+    }
+}
